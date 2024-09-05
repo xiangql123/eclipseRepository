3fab95d24a5e59139a186f60a4574db95117850e
Xiang <2560492930@qq.com>
2024-09-05 18:08:22 +0800

    新的helloWorld

--- learn1/src/ch01/helloWorld.java
+++ learn1/src/ch01/helloWorld.java
@@ -4,7 +4,7 @@ public class helloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world!");
+		System.out.println("hello world!123425231434");
 
 	}
 
